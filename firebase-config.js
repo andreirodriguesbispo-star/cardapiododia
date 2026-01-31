7f5c3d5051e27fe55a78960bfd4cc38e5d08ae0f
andrei rodrigues <andreirodriguesbispo@gmail.com>
2026-01-31 02:24:20 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAipkjePdnsik3C-bEz1yg4L_bbu_nTnKM",
+  authDomain: "cardapiopremium-3f36e.firebaseapp.com",
+  projectId: "cardapiopremium-3f36e",
+  storageBucket: "cardapiopremium-3f36e.firebasestorage.app",
+  messagingSenderId: "272588682804",
+  appId: "1:272588682804:web:0c8f35cf5cdb84b313fc61"
+};
